27765ea2fe2dfd8db4028d176194698e84d38d99
Proenix <piotr26ch@gmail.com>
2015-10-10 12:10:21 +0200

    Apka1: Rozpierducha (notworking)

--- Apka1/Apka1/Form1.Designer.cs
+++ Apka1/Apka1/Form1.Designer.cs
@@ -29,34 +29,62 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.SpringGreen;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(80, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(855, 510);
+            this.pictureBox1.Size = new System.Drawing.Size(787, 454);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 489);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(34, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 454);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(886, 541);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- Apka1/Apka1/Form1.cs
+++ Apka1/Apka1/Form1.cs
@@ -12,10 +12,34 @@ namespace Apka1
 {
     public partial class Form1 : Form
     {
+        // Składniki
+        System.Drawing.Graphics g1; // grafika
+        System.Drawing.Pen p1;  // pióro
+        
+        // Metody
+
+        
+
+        // Eventy
+
         public Form1()
         {
             InitializeComponent();
+            p1 = new System.Drawing.Pen(Color.Blue, 10);
+            g1 = pictureBox1.CreateGraphics();
+
+            p1.Color = Color.Green;
+            p1.Width = 4;
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g1.DrawLine(p1, 1, 1, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // 
+        }
     }
 }
