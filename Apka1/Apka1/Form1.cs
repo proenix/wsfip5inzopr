c668967c1e1d18aa65730b7cdba3459d25f54271
Proenix <piotr26ch@gmail.com>
2015-11-07 12:26:52 +0100

    Apka1: wersja5end

--- Apka1/Apka1/Form1.cs
+++ Apka1/Apka1/Form1.cs
@@ -35,13 +35,13 @@ namespace Apka1
             p1 = new System.Drawing.Pen(Color.Red, 10);
             g1 = pictureBox1.CreateGraphics();  // przydział pikseli do grafiki
 
-            // bufory wspolrzednych punktow krzywej
+            // bufor obiektów ruchomych, obiekt ruchomy jest instancją klasy fig1
             tabFig = new fig1[lfMax];
             int i;
             for (i = 0; i < lfMax; ++i)
             { tabFig[i] = new fig1(); }
             lf = 0; // licznik figur
-            pk1 = new int[lfMax];
+            pk1 = new int[lfMax];  // utworzono lfmaxh obiektów dla obiektów ruchomych
             ipk1 = 0;
 
             // odbijacze, położenie początkowe
@@ -56,13 +56,18 @@ namespace Apka1
             RR.fig.c = Color.Orange;
             RR.pset();
 
+            // tabFig[0] jest piłeczką numer 1
             // punk startu piłeczki nr 1
-            tabFig[0].p[0].X =  LL.X + (int)p1.Width / 2;  // położenie piłeczki, współrzędna pozioma, zależna od położenia i szerokości lewego odbijacza
-            tabFig[0].p[0].Y = pictureBox1.Size.Height - 50;
+            tabFig[0].p[0].X = pictureBox1.Size.Width / 2;  // położenie piłeczki, współrzędna pozioma, zależna od położenia i szerokości lewego odbijacza
+            tabFig[0].p[0].Y = pictureBox1.Size.Height /2;
             // Y - szerokość, wysokość elipsy
+            // romiary i kolor piłeczki nr 1
             p1.Color = Color.Orange;
             tabFig[0].p[1].X = 50;
             tabFig[0].p[1].Y = 50;
+            // kierunek i przesuniećie początkowe piłeczki nr 1
+            tabFig[0].m = 1; // w prawo
+            tabFig[0].k = 2; // renderuj dwa piksele
         }
 
         private void odbijacz(odb o1, int y)
@@ -84,8 +89,8 @@ namespace Apka1
             // współrzędne piłeczki
             // współrzędna X = {x1, y2}   współrzędna Y = {x2, y2}
             // X - lewy góry róg kwadratu, w który będzie wpisana elipsa
-            tabFig[0].p[0].X = LL.X + (int)p1.Width / 2; 
-            tabFig[0].p[0].Y = pictureBox1.Size.Height - 50;
+            tabFig[0].p[0].X = pictureBox1.Size.Width / 2; 
+            tabFig[0].p[0].Y = pictureBox1.Size.Height /2;
 
             p1.Color = Color.Orange;
             g1.DrawEllipse(p1, tabFig[0].p[0].X, tabFig[0].p[0].Y, tabFig[0].p[1].X, tabFig[0].p[1].Y);
@@ -134,8 +139,8 @@ namespace Apka1
             public System.Drawing.Point[] p;
             public char f;
             public Color c;
-            public int m;  // 0 - lewo, 1 - prawo
-            public int k;  // przesunięcie
+            public int m;  // 0 - lewo, 1 - prawo  kierunek ruchu piłeczki 
+            public int k;  // przesunięcie obiektu w pikselach
 
             public fig1()
             {
@@ -164,30 +169,69 @@ namespace Apka1
             p1.Color = pictureBox1.BackColor;
             g1.DrawEllipse(p1, tabFig[0].p[0].X, tabFig[0].p[0].Y, tabFig[0].p[1].X, tabFig[0].p[1].Y);
   
-            // 2. przesuń poziomo
-            tabFig[0].p[0].X = tabFig[0].p[0].X + 5;
+            // 2. przesuń poziomo obiekty ruchome
+            // 2.1 kierunek zmieniany po kolizji z odbijaczem
+            // 2.2 piłeczka nr 1
+            if (tabFig[0].m == 1)  // sprawdzamy kierunek
+                tabFig[0].p[0].X = tabFig[0].p[0].X + tabFig[0].k;
+            else
+                tabFig[0].p[0].X = tabFig[0].p[0].X - tabFig[0].k;
+
             // tabFig[0].p[0].Y = tabFig[0].p[0].Y + 20;
-            // 3. sprawdź przekroczenie krawędzi prawej, lewej
-            if (tabFig[0].p[0].X > pictureBox1.Width) tabFig[0].p[0].X = 20;
+            // 3. sprawdź przekroczenie krawędzi prawej, lewej  piłeczki 1
+            // if (tabFig[0].p[0].X > pictureBox1.Width) tabFig[0].p[0].X = 20;
+
+
             // if (tabFig[0].p[0].Y > pictureBox1.Height) tabFig[0].p[0].Y = 20;
             // 4. rysuj obiekt na picture box
             p1.Color = Color.DarkBlue;
             g1.DrawEllipse(p1, tabFig[0].p[0].X, tabFig[0].p[0].Y, tabFig[0].p[1].X, tabFig[0].p[1].Y);
-            // 5. timer stop jeżeli piłeczka przekroczyła prawą krawędź
-            // sprawdzamy położenie lewej krawędzi piłeczki
+           
+            // 5.1 timer stop jeżeli piłeczka przekroczyła prawą krawędź
+            // sprawdzamy położenie lewej krawędzi piłeczki nr 1 względem prawej krawędzi okienka
             //            zmienna   tabFig[0].p[0].X
-            if (tabFig[0].p[0].X >= pictureBox1.Width) timer1.Stop();
-            // 6. odbicie piłeczki (prawa krawędź) od prawego odbijacza (lewa krwędź)
+            if (tabFig[0].p[0].X - (int)p1.Width / 2 >= pictureBox1.Width) timer1.Stop();
+            // 5.2 timer stop jeżeli piłeczka przekroczyła lewą krawędź
+            // sprawdzamy położenie prawej krawędzi piłeczki nr 1 względem lewej krawędzi okienka
+            //            zmienna   tabFig[0].p[0].X
+            if (tabFig[0].p[0].X + tabFig[0].p[1].X + (int)p1.Width / 2 <= 1) timer1.Stop();
+
+
+            // 6.1 odbicie piłeczki nr 1 (prawa krawędź) od prawego odbijacza (lewa krwędź)
             if (/* wsp. X */ tabFig[0].p[0].X + tabFig[0].p[1].X + (int)p1.Width / 2 >= RR.X - (int)p1.Width / 2)
                 /* wsp. Y - górna, dolna krwędź piłeczki */
-            if ((/* górna krwędź piłeczki */ tabFig[0].p[0].Y - (int)p1.Width/2 >= RR.Y - RR.height/2 /* poniżej */  ) &&
-                 (/* górna krwędź piłeczki */ tabFig[0].p[0].Y - (int)p1.Width/2 <= RR.Y + RR.height/2 /* powyżej */)
-               ||
-                 (/* dolna krwędź piłeczki */ 
-                  tabFig[0].p[0].Y + tabFig[0].p[1].Y  <= RR.Y + RR.height/2) &&
-                 (tabFig[0].p[0].Y + tabFig[0].p[1].Y  >= RR.Y - RR.height/2)
-             )
-                 timer1.Stop();
+                if ((/* górna krwędź piłeczki */ tabFig[0].p[0].Y - (int)p1.Width / 2 >= RR.Y - RR.height / 2 /* poniżej */  ) &&
+                     (/* górna krwędź piłeczki */ tabFig[0].p[0].Y - (int)p1.Width / 2 <= RR.Y + RR.height / 2 /* powyżej */)
+                   ||
+                     (/* dolna krwędź piłeczki */
+                      tabFig[0].p[0].Y + tabFig[0].p[1].Y <= RR.Y + RR.height / 2) &&
+                     (tabFig[0].p[0].Y + tabFig[0].p[1].Y >= RR.Y - RR.height / 2)
+                 )
+                    //timer1.Stop();
+                    // zmień kierunek piłeczki nr 1 na przeciwny
+                    if (tabFig[0].m == 1)
+                        tabFig[0].m = 0;
+                    else
+                        tabFig[0].m = 1;
+              // koniec odbicia piłeczki nr 1
+            // 6.2 odbicie piłeczki nr 1 (lewa krawędź) od lewa odbijacza (lewa krwędź)
+            // tabFig[0].p[0].X - położenie poziome piłeczki,  tabFig[0].p[1].X - szerokość piłeczki, p1.Width - szerokość pióra
+            if (/* wsp. X */ tabFig[0].p[0].X - (int)p1.Width / 2 <= LL.X + (int)p1.Width / 2)
+                /* wsp. Y - górna, dolna krwędź piłeczki */
+                if ((/* górna krwędź piłeczki */ tabFig[0].p[0].Y - (int)p1.Width / 2 >= LL.Y - LL.height / 2 /* poniżej */  ) &&
+                     (/* górna krwędź piłeczki */ tabFig[0].p[0].Y - (int)p1.Width / 2 <= LL.Y + LL.height / 2 /* powyżej */)
+                   ||
+                     (/* dolna krwędź piłeczki */
+                      tabFig[0].p[0].Y + tabFig[0].p[1].Y <= LL.Y + LL.height / 2) &&
+                     (tabFig[0].p[0].Y + tabFig[0].p[1].Y >= LL.Y - LL.height / 2)
+                 )
+                    //timer1.Stop();
+                    // zmień kierunek piłeczki nr 1 na przeciwny
+                    if (tabFig[0].m == 1)
+                        tabFig[0].m = 0;
+                    else
+                        tabFig[0].m = 1;
+            // koniec odbicia piłeczki nr 1
 
 
 
